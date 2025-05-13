6b03d010b7bf464f642a77e2f0b67549b1c63afd
humaixiang <1390521943@qq.com>
2025-05-13 09:29:43 +0800

    变体数量改为200

--- shoptimizer-child-theme/functions.php
+++ shoptimizer-child-theme/functions.php
@@ -98,6 +98,6 @@ foreach ($inc_files as $file) {
 
 
 function custom_wc_ajax_variation_threshold( $qty, $product ) {
-    return 100;
+    return 200;
 }
 add_filter( 'woocommerce_ajax_variation_threshold', 'custom_wc_ajax_variation_threshold', 10, 2 );
